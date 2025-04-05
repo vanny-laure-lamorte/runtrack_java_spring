05c67a7d0db7a2ca56c9676009575aceb259f593
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2025-04-05 19:01:46 +0200

    job03

---
+++ Jour03/Job03/job03/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE person (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+);
---
+++ Jour03/Job03/job03/src/test/java/com/example/runtrack/RuntrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
