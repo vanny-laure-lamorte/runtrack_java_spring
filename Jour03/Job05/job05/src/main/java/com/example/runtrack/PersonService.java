9e5e2da808173748556ff91ef28d1f31020f6234
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2025-04-05 20:18:23 +0200

    job5

---
+++ Jour03/Job05/job05/src/main/java/com/example/runtrack/PersonService.java
@@ -0,0 +1,17 @@
+package com.example.runtrack;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+}
