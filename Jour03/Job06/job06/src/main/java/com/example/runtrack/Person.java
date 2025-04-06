3e9030ca8ec6b60fd70c96f483040a1b82c1a300
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2025-04-06 14:28:15 +0200

    job06 test lombok

--- Jour03/Job06/job06/src/main/java/com/example/runtrack/Person.java
+++ Jour03/Job06/job06/src/main/java/com/example/runtrack/Person.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
 
 @Entity
+@Data
 public class Person {
 
     @Id
@@ -20,26 +22,7 @@ public class Person {
         this.name = name;
         this.age = age;
     }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-
+    
     @Override
     public String toString() {
         return "Person{id=" + id + ", name='" + name + "', age=" + age + "}";
